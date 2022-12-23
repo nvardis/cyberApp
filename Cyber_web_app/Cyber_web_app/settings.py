f2a08ca452df531e7f89a2a5f676a18cca814ba9
orad <orad88@gmail.com>
2022-12-23 12:12:09 +0200

    changes

--- Cyber_web_app/Cyber_web_app/settings.py
+++ Cyber_web_app/Cyber_web_app/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Cyber_web_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
